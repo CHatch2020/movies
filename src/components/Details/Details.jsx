b83465065b7e90c8698eb624d7579daa6c1a3a81
Caleb Hatch <hatchcaleb@gmail.com>
2021-12-12 21:02:29 -0600

    finished styling

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,22 +1,27 @@
 import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import "./Details.css";
 
 export default function Details() {
   const movie = useSelector((store) => store.selectedMovie);
   const history = useHistory();
 
   const goBack = () => {
-      history.push('/');
-  }
+    history.push("/");
+  };
 
   return (
     <div>
-      <h1>Selected Movie</h1>
-      <img src={movie.poster} alt={movie.title} />
-      <h2>{movie.title}</h2>
-      <h4>{movie.name}</h4>
-      <h5>{movie.description}</h5>
-      <button onClick={goBack}>Home</button>
+      <div className="head">
+        <h1>Selected Movie</h1>
+      </div>
+      <img className="image" src={movie.poster} alt={movie.title} />
+      <div className="info">
+        <h2 className="title">{movie.title}</h2>
+        <h4 className="genre">{movie.name}</h4>
+        <h5 className="desc">{movie.description}</h5>
+      </div>
+      <button className="homeButt" onClick={goBack}>Home</button>
     </div>
   );
 }
